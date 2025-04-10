499ed245bc0d5f84d233080fe79e678b57cec34a
zhuwangyi <z00699601@china.huawei.com>
2025-04-10 12:01:28 +0800

    test_team例子跑通

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,21 +2,19 @@
 Current_path=$(pwd)
 cd ..
 
+rm -rf test_scalar_p
+cp ./build/bin/test_scalar_p ./
+
 rm -rf team_example
 cp ./build/bin/team_example ./
 
-export LD_LIBRARY_PATH=$(pwd)/install/lib:${ASCEND_HOME_PATH}/lib64:$(pwd)/3rdparty/output:$LD_LIBRARY_PATH
-# mpirun --allow-run-as-root -n 8 ./team_example
-
 RANK_SIZE="8"
 IP_PORT="tcp://127.0.0.1:8666"
-echo " ====== example run, ranksize: ${RANK_SIZE} ip: ${IP_PORT} ======"
-
-rm -f test_scalar_p
-cp ./build/bin/test_scalar_p ./
+export LD_LIBRARY_PATH=$(pwd)/install/lib:${ASCEND_HOME_PATH}/lib64:$(pwd)/3rdparty/output:$LD_LIBRARY_PATH
 
 for (( idx = 0; idx < ${RANK_SIZE}; idx = idx + 1 )); do
     ./test_scalar_p ${RANK_SIZE} ${idx} ${IP_PORT} &
+    # ./team_example ${RANK_SIZE} ${idx} ${IP_PORT} &
 done
 
 cd ${Current_path}
--- tests/test_team/team_example.cpp
+++ tests/test_team/team_example.cpp
@@ -1,59 +1,79 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
-#include <acl/acl.h>
-
 using namespace std;
 
-#include <mpi.h>
-#include <mutex>
-#include <vector>
+#include <acl/acl.h>
+#include "data_utils.h"
 
-#include "runtime/kernel.h"
-#include "runtime/mem.h"
-#include "runtime/dev.h"
+#include "smem.h"
+#include "smem_shm.h"
 
 #include "init.h"
 
-int main(int argc, char** argv) {
+static uint32_t gNpuNum = 8;
+static uint64_t gNpuMallocSpace = 1024UL * 1024UL * 1024;
 
-    aclInit(nullptr);
+int main(int argc, char* argv[]) 
+{
+    int rankSize = atoi(argv[1]);
+    int rankId = atoi(argv[2]);
+    std::string ipport = argv[3];
+    std::cout << "[TEST] input rank_size: " << rankSize << " rank_id:" << rankId << " input_ip: " << ipport << std::endl;
 
-    MPI_Init(&argc, &argv);
+    if (rankSize != (rankSize & (~(rankSize - 1)))) {
+        std::cout << "[TEST] input rank_size: "<< rankSize << " is not the power of 2" << std::endl;
+        return -1;
+    }
 
-    int rank, size;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    CHECK_ACL(aclInit(nullptr));
+    int32_t deviceId = rankId % gNpuNum;
+    CHECK_ACL(aclrtSetDevice(deviceId));
+    aclrtStream stream = nullptr;
+    CHECK_ACL(aclrtCreateStream(&stream));
 
-    aclrtSetDevice(rank);
+    auto ret = smem_init(gNpuMallocSpace * rankSize, 0);
+    if (ret != 0) {
+        ERROR_LOG("[TEST] shm init failed, ret:%d, rankId:%d \n", ret, rankId);
+        return -1;
+    }
+    uint32_t flags = 0;
+    void *gva = nullptr;
+    smem_shm_t handle = smem_shm_create(0, ipport.c_str(), rankSize, rankId, deviceId, gNpuMallocSpace, SMEMS_DATA_OP_MTE, flags, &gva);
+    if (handle == nullptr || gva == nullptr) {
+        ERROR_LOG("[TEST] smem_shm_create failed, rankId:%d \n", rankId);
+        return -1;
+    }
+    WARN_LOG("[TEST] smem_shm_create gva %p, size %lu, rankId:%d \n", gva, gNpuMallocSpace, rankId);
 
-    string pFlag = "[Process " + to_string(rank) + "] ";
-
-    shmem_init(rank, size);
+    shmem_init(rankId, rankSize);
 
     // #################### 子通信域切分测试 ############################
     shmem_team_t team_odd, team_even;
     int start = 1;
     int stride = 2;
     int team_size = 4;
-    auto status = shmem_team_split_strided(SHMEM_TEAM_WORLD, start, stride, team_size, team_odd);
+    shmem_team_split_strided(SHMEM_TEAM_WORLD, start, stride, team_size, team_odd);
 
     // #################### host侧取值测试 ##############################
+    string pFlag = "[Process " + to_string(rankId) + "] ";
     std::cout << pFlag << "shmem_team_n_pes(team_odd): " << shmem_team_n_pes(team_odd) << std::endl;
     std::cout << pFlag << "shmem_team_mype(team_odd): " << shmem_team_mype(team_odd) << std::endl;
     std::cout << pFlag << "shmem_n_pes(): " << shmem_n_pes() << std::endl;
     std::cout << pFlag << "shmem_mype(): " << shmem_mype() << std::endl;
 
     // 保证前序子team创建完成，有个全局数组
-    MPI_Barrier(MPI_COMM_WORLD);
+    // xx_Barrier(SHMEM_TEAM_WORLD);
+    sleep(2);
 
     start = 0;
     stride = 2;
     team_size = 4;
-    status = shmem_team_split_strided(SHMEM_TEAM_WORLD, start, stride, team_size, team_even);
+    shmem_team_split_strided(SHMEM_TEAM_WORLD, start, stride, team_size, team_even);
 
     // 保证子team创建完成
-    MPI_Barrier(MPI_COMM_WORLD);
+    // xx_Barrier(SHMEM_TEAM_WORLD);
+    sleep(2);
 
     std::cout << pFlag << "shmem_team_translate_pe(team_even, 2, SHMEM_TEAM_WORLD): " << shmem_team_translate_pe(team_even, 2, SHMEM_TEAM_WORLD) << std::endl;
 
@@ -62,7 +82,9 @@ int main(int argc, char** argv) {
     shmem_team_destroy(team_even);
 
     shmem_finalize();
-    MPI_Finalize();
-    aclFinalize();
+    smem_shm_destroy(handle, flags);
+    CHECK_ACL(aclrtDestroyStream(stream));
+    CHECK_ACL(aclrtResetDevice(deviceId));
+    CHECK_ACL(aclFinalize());
     return 0;
 }
