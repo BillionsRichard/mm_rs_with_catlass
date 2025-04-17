81b2733e24dbb427ce58e6ed316a5e42049a2288
zhuwangyi <z00699601@china.huawei.com>
2025-04-17 19:22:04 +0800

    Some Merge Code Revert

--- include/host/types.h
+++ include/host/types.h
@@ -10,29 +10,6 @@
 
 #include "shmem_internal.h"
 
-#define STATE_SCALAR_INVALID -1
-
-#define SHMEM_TEAM_INITALIZER                                                         \
-    {                                                                                 \
-        (1 << 16) + sizeof(ShmemDeviceHostStateT), /* version */                      \
-    }
-
-#define SHMEM_DEVICE_HOST_STATE_INITALIZER                                            \
-    {                                                                                 \
-        (1 << 16) + sizeof(ShmemDeviceHostStateT),  /* version */                     \
-            STATE_SCALAR_INVALID,                    /* mype */                       \
-            STATE_SCALAR_INVALID,                    /* npes */                       \
-            NULL,                                    /* heapBase */                   \
-            {NULL},                                  /* p2pHeapBase */                \
-            SIZE_MAX,                                /* heapSize */                   \
-            {NULL},                                  /* teamPools */                  \
-            NULL,                                    /* psyncPool */                  \
-            NULL,                                    /* syncCounter */                \
-            false,                                   /* shmem_is_shmem_initialized */ \
-            false,                                   /* shmem_is_shmem_created */     \
-            {0, 0, 0},                               /* shmem_mte_config */           \
-    }
-
 // attr
 typedef struct {
     int myRank;
