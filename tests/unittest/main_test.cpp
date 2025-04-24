8b4186e6b7f1250ecceca96b0126e962c1ce2ee3
zhangyunqi05 <zhangyunqi5@huawei.com>
2025-04-24 20:46:32 +0800

    bugfix

--- tests/unittest/main_test.cpp
+++ tests/unittest/main_test.cpp
@@ -1,13 +1,9 @@
 #include <gtest/gtest.h>
 #include <iostream>
 
-#define DEFAULT_RANKS 8
-#define DEFAULT_NPU_NUM 8
-#define DEFAULT_IPPORT "tcp://127.0.0.1:8666"
-
-int testGlobalRanks = DEFAULT_RANKS;
-int testGNpuNum = DEFAULT_NPU_NUM;
-const char* testGlobalIpport = DEFAULT_IPPORT;
+int testGlobalRanks;
+int testGNpuNum;
+const char* testGlobalIpport;
 
 void TestMutilTask(std::function<void(int, int, uint64_t)> func, uint64_t localMemSize, int processCount){
     pid_t pids[processCount];
