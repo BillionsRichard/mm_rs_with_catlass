730c1a26034023a20180a18ca276d219d3c2af8c
zhuwangyi <z00699601@china.huawei.com>
2025-05-06 11:29:44 +0800

    Change example build script && template kernel files name

--- examples/matmul_allreduce/build.sh
+++ examples/matmul_allreduce/build.sh
@@ -1,10 +1,18 @@
+#!/bin/bash
+CURRENT_DIR=$(pwd)
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+PROJECT_ROOT=$( dirname $(dirname "$SCRIPT_DIR"))
+
+echo ${PROJECT_ROOT}
+
 MEMFABRIC_INCLUDE_PATH=$PROJECT_ROOT/3rdparty/memfabric_hybrid/include/smem/
 MEMFABRIC_LIB_PATH=$PROJECT_ROOT/3rdparty/memfabric_hybrid/lib/
 
 SHMEM_INCLUDE_PATH=$PROJECT_ROOT/include/
 SHMEM_LIB_PATH=$PROJECT_ROOT/install/lib/
 
-cd examples/matmul_allreduce
+cd ${SCRIPT_DIR}
+
 mkdir -p out
 ccec -O2 -std=c++17 -xcce --cce-aicore-arch=dav-c220                \
     -mllvm -cce-aicore-stack-size=0x8000                            \
@@ -20,17 +28,16 @@ ccec -O2 -std=c++17 -xcce --cce-aicore-arch=dav-c220                \
     -I${ASCEND_HOME_PATH}/include                                   \
     -I${ASCEND_HOME_PATH}/include/experiment/runtime                \
     -I${ASCEND_HOME_PATH}/include/experiment/msprof                 \
+    -I$PROJECT_ROOT/3rdparty/ascendc-templates/include/             \
     -I$PROJECT_ROOT/3rdparty/ascendc-templates/examples/common      \
-    -I$PROJECT_ROOT/examples/include/                               \
     -I$PROJECT_ROOT/include/                                        \
     -I$MEMFABRIC_INCLUDE_PATH/host/                                 \
     -I$MEMFABRIC_INCLUDE_PATH/device/                               \
     -I$SHMEM_INCLUDE_PATH/host/                                     \
     -I$SHMEM_INCLUDE_PATH/host_device/                              \
     -I$SHMEM_INCLUDE_PATH/device/                                   \
-    -I$PROJECT_ROOT/3rdparty/ascendc-templates/include/             \
-    -I..                                                            \
-    -I.                                                             \
+    -I../                                                           \
+    -I./                                                            \
     -L${ASCEND_HOME_PATH}/lib64                                     \
     -L$MEMFABRIC_LIB_PATH/ -lmf_smem -lmf_hybm_core                 \
     -L$SHMEM_LIB_PATH/ -lshmem_host -lshmem_device                  \
@@ -38,3 +45,5 @@ ccec -O2 -std=c++17 -xcce --cce-aicore-arch=dav-c220                \
     -lruntime -lstdc++ -lascendcl -lm -ltiling_api                  \
     -lplatform -lc_sec -ldl -lnnopbase                              \
     main.cpp -o out/matmul_allreduce
+
+cd ${CURRENT_DIR}
