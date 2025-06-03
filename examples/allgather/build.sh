ac479eff35d254a54c043bb63b06c05e54091132
zhangyunqi05 <zhangyunqi5@huawei.com>
2025-06-03 22:05:48 +0800

    fix

--- examples/allgather/build.sh
+++ examples/allgather/build.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 PROJECT_ROOT=$( dirname $(dirname "$SCRIPT_DIR"))
 
 BUILD_DIR="${SCRIPT_DIR}/build"
-rm -rf BUILD_DIR
+rm -rf $BUILD_DIR
 if [ ! -d "$BUILD_DIR" ]; then
     mkdir "$BUILD_DIR"
 fi
