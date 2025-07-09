ea3595063839b6a411aa4bbbed64e41caef6e5f7
xiebin <xiebin18@huawei.com>
2025-07-09 10:59:18 +0800

    bug fix

--- src/host/under_api/mf_hybrid/smem_shm_api.cpp
+++ src/host/under_api/mf_hybrid/smem_shm_api.cpp
@@ -8,7 +8,6 @@
  * See LICENSE in the root of the software repository for the full text of the License.
  */
 #include <dlfcn.h>
-#include "smem_shm_api.h"
 #include "shmemi_host_common.h"
 
 namespace shm {
