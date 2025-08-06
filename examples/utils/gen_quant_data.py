3162c37cfb2ba0a29c64097d0b873d330deefd01
BillionsRichard <billions.richard@qq.com>
2025-08-06 11:37:19 +0800

    Update gen_quant_data.py

--- examples/utils/gen_quant_data.py
+++ examples/utils/gen_quant_data.py
@@ -6,7 +6,9 @@ def gen_random_data(size, dtype):
     if dtype == torch.float16 or dtype == torch.bfloat16 or dtype == torch.float32:
         return torch.randn(size=size, dtype=dtype)
     elif dtype == torch.int8:
-        return torch.randint(-16, 16, size=size, dtype=dtype)
+        # return torch.randint(-16, 16, size=size, dtype=dtype)
+        # for debug use all ones.
+        return torch.ones(size=size, dtype=dtype)
     else:
         print(f"Invalid dtype: {dtype}.")
         exit(1)
@@ -16,7 +18,7 @@ def gen_golden_data():
     parser = argparse.ArgumentParser()
     parser.add_argument('comm_type', type=CommType.from_str,
                         choices=[CommType.MATMUL_ALLREDUCE, CommType.ALLGATHER_MATMUL, CommType.MATMUL_REDUCE_SCATTER])
-    parser.add_argument('out_dtype', type=DataType.from_str, choices=[DataType.FLOAT16, DataType.BF16]) # [fp16:1, bf16:27]
+    parser.add_argument('out_dtype', type=DataType.from_str, choices=[DataType.FLOAT16, DataType.BF16])
     parser.add_argument('rank_size', type=int)
     parser.add_argument('m', type=int)
     parser.add_argument('n', type=int)
@@ -30,11 +32,11 @@ def gen_golden_data():
     x1_gm = gen_random_data([M, K], dtype=torch.int8)
     x2_gm = gen_random_data([K, N], dtype=torch.int8)
     # scale_x1_gm = torch.randn(M, dtype=torch.float32) * 0.01
-    # scale_x2_gm = torch.randn(N, dtype=torch.float32) * 0.01    
+    # scale_x2_gm = torch.randn(N, dtype=torch.float32) * 0.01
+    # for debug
     scale_x1_gm = torch.ones(size=(M,), dtype=torch.float32) * 0.01
     scale_x2_gm = torch.ones(size=(N,), dtype=torch.float32) * 0.01
-    # bias_gm = torch.randint(-16, 16, size=(N,), dtype=torch.int32)
-    bias_gm = torch.zeros(size=(N,), dtype=torch.int32)
+    bias_gm = torch.zeros(N, dtype=torch.int32)
     c_gm = torch.zeros((M // args.rank_size, N), dtype=args.out_dtype.torch_type)
 
     # Calculate golden result
@@ -42,7 +44,7 @@ def gen_golden_data():
     accumulator = torch.matmul(x1_gm.to(l0c_dtype), x2_gm.to(l0c_dtype))
     
     dequantized = accumulator + bias_gm.to(l0c_dtype)
-    
+
     # Apply scales
     # scale_x1 is per-token (per-row of A), scale_x2 is per-channel (per-column of B)
     result_fp32 = dequantized * scale_x1_gm.unsqueeze(1).to(l0c_dtype) * scale_x2_gm.unsqueeze(0).to(l0c_dtype)
@@ -52,20 +54,20 @@ def gen_golden_data():
     for _ in range(args.rank_size):
         golden += result_fp32
     
-    golden = golden.to(args.out_dtype.torch_type)
+    # golden = golden.to(args.out_dtype.torch_type)
 
     if args.transA:
         x1_gm = x1_gm.transpose(0, 1).contiguous()
     if args.transB:
         x2_gm = x2_gm.transpose(0, 1).contiguous()
 
-    tensor_to_file(x1_gm, "./output/x1_gm.bin") # int8
-    tensor_to_file(x2_gm, "./output/x2_gm.bin") # int 8
-    tensor_to_file(scale_x1_gm, "./output/scale_x1_gm.bin") # fp32
-    tensor_to_file(scale_x2_gm, "./output/scale_x2_gm.bin") # fp32
-    tensor_to_file(bias_gm, "./output/bias_gm.bin") # int32
+    tensor_to_file(x1_gm, "./output/x1_gm.bin")
+    tensor_to_file(x2_gm, "./output/x2_gm.bin")
+    tensor_to_file(scale_x1_gm, "./output/scale_x1_gm.bin")
+    tensor_to_file(scale_x2_gm, "./output/scale_x2_gm.bin")
+    tensor_to_file(bias_gm, "./output/bias_gm.bin")
     tensor_to_file(c_gm, "./output/c_gm.bin") # This is a placeholder for the output buffer
-    tensor_to_file(golden, "./output/golden.bin") # fp16
+    tensor_to_file(golden, "./output/golden.bin")
 
 if __name__ == '__main__':
     gen_golden_data()
